f92df9ac8abcdd7d755509ecd4755743266f8782
Ahmad Faris <isfawwaz@gmail.com>
2020-09-17 16:05:15 +0700

    Update core - wordpress 5.5.1

--- wp-config-sample.php
+++ wp-config-sample.php
@@ -1,44 +1,90 @@
 <?php
-// ** MySQL settings ** //
+/**
+ * The base configuration for WordPress
+ *
+ * The wp-config.php creation script uses this file during the
+ * installation. You don't have to use the web site, you can
+ * copy this file to "wp-config.php" and fill in the values.
+ *
+ * This file contains the following configurations:
+ *
+ * * MySQL settings
+ * * Secret keys
+ * * Database table prefix
+ * * ABSPATH
+ *
+ * @link https://wordpress.org/support/article/editing-wp-config-php/
+ *
+ * @package WordPress
+ */
 
-/** The prefix of table */
-define( 'CONFIG_DB_PREFIX', 'DATABASE_PREFIX' );
-
+// ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'CONFIG_DB_NAME', 'DATABASE_NAME' );
+define( 'DB_NAME', 'database_name_here' );
 
 /** MySQL database username */
-define( 'CONFIG_DB_USER', 'DATABASE_USER' );
+define( 'DB_USER', 'username_here' );
 
 /** MySQL database password */
-define( 'CONFIG_DB_PASSWORD', 'DATABASE_PASSWORD' );
+define( 'DB_PASSWORD', 'password_here' );
 
 /** MySQL hostname */
-define( 'CONFIG_DB_HOST', 'localhost' );
+define( 'DB_HOST', 'localhost' );
 
 /** Database Charset to use in creating database tables. */
-define( 'CONFIG_DB_CHARSET', 'utf8' );
+define( 'DB_CHARSET', 'utf8' );
 
 /** The Database Collate type. Don't change this if in doubt. */
-define( 'CONFIG_DB_COLLATE', '' );
+define( 'DB_COLLATE', '' );
 
-/** Disable file edit through dashboard */
-define( 'DISALLOW_FILE_EDIT', true );
+/**#@+
+ * Authentication Unique Keys and Salts.
+ *
+ * Change these to different unique phrases!
+ * You can generate these using the {@link https://api.wordpress.org/secret-key/1.1/salt/ WordPress.org secret-key service}
+ * You can change these at any point in time to invalidate all existing cookies. This will force all users to have to log in again.
+ *
+ * @since 2.6.0
+ */
+define( 'AUTH_KEY',         'put your unique phrase here' );
+define( 'SECURE_AUTH_KEY',  'put your unique phrase here' );
+define( 'LOGGED_IN_KEY',    'put your unique phrase here' );
+define( 'NONCE_KEY',        'put your unique phrase here' );
+define( 'AUTH_SALT',        'put your unique phrase here' );
+define( 'SECURE_AUTH_SALT', 'put your unique phrase here' );
+define( 'LOGGED_IN_SALT',   'put your unique phrase here' );
+define( 'NONCE_SALT',       'put your unique phrase here' );
 
-/** Disabled automatic update */
-define( 'AUTOMATIC_UPDATER_DISABLED', true );
+/**#@-*/
 
-// ** DEFINE DEBUG ** //
+/**
+ * WordPress Database Table prefix.
+ *
+ * You can have multiple installations in one database if you give each
+ * a unique prefix. Only numbers, letters, and underscores please!
+ */
+$table_prefix = 'wp_';
 
-// Enable WP_DEBUG mode
+/**
+ * For developers: WordPress debugging mode.
+ *
+ * Change this to true to enable the display of notices during development.
+ * It is strongly recommended that plugin and theme developers use WP_DEBUG
+ * in their development environments.
+ *
+ * For information on other constants that can be used for debugging,
+ * visit the documentation.
+ *
+ * @link https://wordpress.org/support/article/debugging-in-wordpress/
+ */
 define( 'WP_DEBUG', false );
 
-// Enable Debug logging to the /wp-content/debug.log file
-define( 'WP_DEBUG_LOG', false );
+/* That's all, stop editing! Happy publishing. */
 
-// Disable display of errors and warnings
-define( 'WP_DEBUG_DISPLAY', false );
-@ini_set( 'display_errors', 0 );
+/** Absolute path to the WordPress directory. */
+if ( ! defined( 'ABSPATH' ) ) {
+	define( 'ABSPATH', __DIR__ . '/' );
+}
 
-// Use dev versions of core JS and CSS files (only needed if you are modifying these core files)
-define( 'SCRIPT_DEBUG', false );
+/** Sets up WordPress vars and included files. */
+require_once ABSPATH . 'wp-settings.php';
