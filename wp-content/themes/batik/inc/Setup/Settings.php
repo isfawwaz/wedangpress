c57a0191aac3cc2a37dfed601ffc4ffded0ffe98
Ahmad Faris <isfawwaz@gmail.com>
2021-04-20 22:33:28 +0700

    fix: theme settings to default

--- wp-content/themes/batik/inc/Setup/Settings.php
+++ wp-content/themes/batik/inc/Setup/Settings.php
@@ -37,52 +37,52 @@ class Settings {
     }
 
     public function admin_field_company( $options ) {
-        $addon = [];
+        // $addon = [];
 
-        // Unset field tagline
-        unset( $options['tagline'] );
-        unset( $options['fax'] );
+        // // Unset field tagline
+        // unset( $options['tagline'] );
+        // unset( $options['fax'] );
 
-        // Change phone number label
-        $options['phone']['title'] = __('Phone Number - Karaoke', 'lorasin');
-        $options['phone']['args']['description'] = __('Your karaoke official phone line', 'lorasin');
+        // // Change phone number label
+        // $options['phone']['title'] = __('Phone Number - Karaoke', 'lorasin');
+        // $options['phone']['args']['description'] = __('Your karaoke official phone line', 'lorasin');
 
-        // Add custom phone number hotel
-        $addon[] = [
-            'id' => 'company_phone_hotel',
-            'title' => __( 'Phone Number - Hotel', 'lorasin' ),
-            'callback' => 'field_render',
-            'page' => 'theme_settings',
-            'section' => 'theme_setting_company_information',
-            'args' => [
-                'option' => 'company_phone_hotel',
-                'name' => 'company_phone_hotel',
-                'placeholder' => __( '+62-8XX-XXXX-XXXX', 'lorasin' ),
-                'description' => __( 'Your hotel official phone line', 'lorasin' ),
-            ],
-        ];
+        // // Add custom phone number hotel
+        // $addon[] = [
+        //     'id' => 'company_phone_hotel',
+        //     'title' => __( 'Phone Number - Hotel', 'lorasin' ),
+        //     'callback' => 'field_render',
+        //     'page' => 'theme_settings',
+        //     'section' => 'theme_setting_company_information',
+        //     'args' => [
+        //         'option' => 'company_phone_hotel',
+        //         'name' => 'company_phone_hotel',
+        //         'placeholder' => __( '+62-8XX-XXXX-XXXX', 'lorasin' ),
+        //         'description' => __( 'Your hotel official phone line', 'lorasin' ),
+        //     ],
+        // ];
 
-        // Change email label
-        $options['email']['title'] = __('Email - Karaoke', 'lorasin');
-        $options['email']['args']['description'] = __('Your karaoke official email account', 'lorasin');
+        // // Change email label
+        // $options['email']['title'] = __('Email - Karaoke', 'lorasin');
+        // $options['email']['args']['description'] = __('Your karaoke official email account', 'lorasin');
 
-        // Add custom email hotel
-        $addon[] = [
-            'id' => 'company_email_hotel',
-            'title' => __( 'Email - Hotel', 'lorasin' ),
-            'callback' => 'field_render',
-            'page' => 'theme_settings',
-            'section' => 'theme_setting_company_information',
-            'args' => [
-                'option' => 'company_email_hotel',
-                'name' => 'company_email_hotel',
-                'placeholder' => __( 'hi@company.domain', 'lorasin' ),
-                'description' => __( 'Your hotel official email account', 'lorasin' ),
-            ],
-        ];
+        // // Add custom email hotel
+        // $addon[] = [
+        //     'id' => 'company_email_hotel',
+        //     'title' => __( 'Email - Hotel', 'lorasin' ),
+        //     'callback' => 'field_render',
+        //     'page' => 'theme_settings',
+        //     'section' => 'theme_setting_company_information',
+        //     'args' => [
+        //         'option' => 'company_email_hotel',
+        //         'name' => 'company_email_hotel',
+        //         'placeholder' => __( 'hi@company.domain', 'lorasin' ),
+        //         'description' => __( 'Your hotel official email account', 'lorasin' ),
+        //     ],
+        // ];
 
-        // Merge array
-        array_splice( $options, 2, 0, $addon );
+        // // Merge array
+        // array_splice( $options, 2, 0, $addon );
 
         return $options;
     }
